a6aa7ed993dd50b68bedca15fef47e4d3a0e7a47
halllllll <h4l@yahoo.ne.jp>
2018-08-01 13:33:50 +0900

    conditional operator

---
+++ kakisute/conditional_op.rb
@@ -0,0 +1,13 @@
+def greet(country)
+    if country=='japan'
+        puts 'んちゃ'
+    elsif country=='us'
+        puts 'hello'
+    elsif country=='italy'
+        puts 'ciao'
+    else
+        puts '???'
+    end
+end
+
+greet('italy')
