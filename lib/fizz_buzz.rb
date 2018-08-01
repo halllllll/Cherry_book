0172a50802d240590eaa264d0b90496336274bf6
halllllll <h4l@yahoo.ne.jp>
2018-08-01 14:20:17 +0900

    fizz_buzz hard manual testing👹

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,1 +1,12 @@
-puts "hello coding time"
+def fizz_buzz(n)
+    if n % 3==0
+        'Fizz'
+    else
+        n.to_s
+    end
+end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(15)
