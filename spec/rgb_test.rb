4746358af9c5c87767dc614d4105506bad7ee070
halllllll <h4l@yahoo.ne.jp>
2018-08-03 13:23:15 +0900

    test first

--- spec/rgb_test.rb
+++ spec/rgb_test.rb
@@ -4,5 +4,6 @@ RSpec.describe 'RGB変換' do
     example 'to_hex' do
         expect(to_hex(0, 0, 0)).to eq '#000000'
         expect(to_hex(255, 255, 255)).to eq '#ffffff'
+        expect(to_hex(4, 60, 120)).to eq '#043c78'
     end
 end
