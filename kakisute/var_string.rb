824fce893deea67dc1677e0e0bd1e5763f4e4456
halllllll <h4l@yahoo.ne.jp>
2018-08-04 11:52:54 +0900

    remove cache for edited gitignore

--- kakisute/var_string.rb
+++
@@ -1,7 +0,0 @@
-# 特殊文字を使うときはダブルクォート
-puts "FINALFANT\nASY"
-# シングルクォートだと単に文字列として評価される
-puts 'FINALFANT\nASY'
-# 式展開もダブルクォートのみ
-name = 'ruby'
-puts "hello, #{name}"
--- lib/rgb.rb
+++
@@ -1,7 +0,0 @@
-def to_hex(r, g, b)
-    [r, g, b].inject('#'){|result, c|result+c.to_s(16).rjust(2, '0')}
-end
-
-def to_ints(hex)
-    hex.scan(/\w\w/).map(&:hex)
-end
