41feedacc501ea078e9e07a1167b239188e03d0f
halllllll <h4l@yahoo.ne.jp>
2018-08-04 12:24:15 +0900

    refectoring test case and source

--- spec/convert_length_spec.rb
+++ spec/convert_length_spec.rb
@@ -1,8 +1,8 @@
 require 'convert_length'
 RSpec.describe '尺貫法ですよね' do
     example 'convert_length' do
-        expect(convert_length(1, :m, :in)).to eq 39.37
-        expect(convert_length(15, :in, :m)).to eq 0.38
-        expect(convert_length(35000, :ft, :m)).to eq 10670.73
+        expect(convert_length(1, from: :m, to: :in)).to eq 39.37
+        expect(convert_length(15, from: :in, to: :m)).to eq 0.38
+        expect(convert_length(35000, from: :ft, to: :m)).to eq 10670.73
     end
 end
