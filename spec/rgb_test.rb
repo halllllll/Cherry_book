d22c8151d977faa92ee4d8c43bec6d2a4323ec77
halllllll <h4l@yahoo.ne.jp>
2018-08-04 11:40:01 +0900

    while operator

--- spec/rgb_test.rb
+++ spec/rgb_test.rb
@@ -6,4 +6,10 @@ RSpec.describe 'RGB変換' do
         expect(to_hex(255, 255, 255)).to eq '#ffffff'
         expect(to_hex(4, 60, 120)).to eq '#043c78'
     end
+    example 'to_ints' do
+        expect(to_ints('#000000')).to eq [0, 0, 0]
+        expect(to_ints('#ffffff')).to eq [255, 255, 255]
+        expect(to_ints('#043c78')).to eq [4, 60, 120]
+    end
+
 end
