146b8663689e458e34a9f62c014cc161882cd94b
halllllll <h4l@yahoo.ne.jp>
2018-08-04 12:09:38 +0900

    hash and symbol

---
+++ kakisute/hash_experiment.rb
@@ -0,0 +1,7 @@
+# rubyではhashのkeyはシンボルがよく使われる
+# keyを文字列にした場合
+dict = {'japan'=>'jp', 'americak'=>'us', 'china'=>'cn'}
+p dict
+# シンボルにした場合
+dict = {'japan':'jp', 'amerika':'us', 'china':'cn'}
+p dict
