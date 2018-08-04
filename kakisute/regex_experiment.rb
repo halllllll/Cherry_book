6d97dc8400cefbc7827767bdf81911728394cb21
halllllll <h4l@yahoo.ne.jp>
2018-08-04 18:34:20 +0900

    regex object and string

--- kakisute/regex_experiment.rb
+++ kakisute/regex_experiment.rb
@@ -5,3 +5,10 @@ Pyhon is a great lunguage.
 Java and Javascript are different.
 TEXT
 puts text.scan(/[A-Z][A-Za-z]+/)
+
+regex = /\d{3}-\d{4}/
+puts regex.class
+
+# if matches...
+s = '123-11, 238382, 242-5893あばば 503-241-395 yoyo 翼を与えよ111-9999 9822 fd938'
+puts s=~regex
