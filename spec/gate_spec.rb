e48db5b3d3c6355aabbe88e949d3d29ed6011033
halllllll <h4l@yahoo.ne.jp>
2018-08-17 12:40:41 +0900

    そんで失敗するやつ書くじゃないですか

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../kakisute/class_experimence/7.4/gate'
-# require '/Users/hal/Documents/workspace/cherry_book/Cherry_book/kakisute/class_experimence/7.4/gate.rb'
 RSpec.describe Gate do
     subject(:gate){Gate.new}
     it 'should be created' do
