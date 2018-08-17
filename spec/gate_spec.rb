5398372af45cfd8109d96dc77a48843cfad3417e
halllllll <h4l@yahoo.ne.jp>
2018-08-17 12:46:54 +0900

    そんでtickeクラス作って初期化処理して、specから呼び出せるようにするじゃないですか。そしたら今度はenterメソッドがねぇっつって言われるんすよね

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,7 +1,12 @@
 require_relative '../kakisute/class_experimence/7.4/gate'
+require_relative '../kakisute/class_experimence/7.4/ticket'
 RSpec.describe Gate do
-    subject(:gate){Gate.new}
+    subject(:umeda){Gate.new(:umeda)}
+    subject(:juso){Gate.new(:juso)}
+    
+    subject(:ticket){Ticket.new(150)}
     it 'should be created' do
-        puts gate.class
+        umeda.enter(ticket)
+        expect(juso.exit(ticket)).to be_true
     end
 end
