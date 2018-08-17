90b1970dafe06f1f5e782d3b86dab1eedf4f3b68
halllllll <h4l@yahoo.ne.jp>
2018-08-17 13:11:35 +0900

    で、明らかに駄目なパターンを追加してみると、当然trueを返しやがるから、意図した結果にならないことを確認できるんですよね。リファクタリングのターンdesu

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -3,10 +3,18 @@ require_relative '../kakisute/class_experimence/7.4/ticket'
 RSpec.describe Gate do
     subject(:umeda){Gate.new(:umeda)}
     subject(:juso){Gate.new(:juso)}
-    
+    subject(:mikuni){Gate.new(:mikuni)}
+
     subject(:ticket){Ticket.new(150)}
     it 'should be created' do
         umeda.enter(ticket)
         expect(juso.exit(ticket)).to be true
+    end
+
+    context 'from umeda to mikuni when fare is not enough' do
+        it 'can\'t exit'  do
+            umeda.enter(ticket)
+            expect(mikuni.exit(ticket)).not_to be true
+        end
     end
 end
