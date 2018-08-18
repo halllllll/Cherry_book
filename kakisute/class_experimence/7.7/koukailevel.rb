e313dbed963e9cb37ba7e588730a3f5d3aeaa340
halllllll <h4l@yahoo.ne.jp>
2018-08-18 23:08:12 +0900

    initializeメソッド以外はデフォルトでpublicになる

--- kakisute/class_experimence/7.7/koukailevel.rb
+++ kakisute/class_experimence/7.7/koukailevel.rb
@@ -1,5 +1,13 @@
 # initializeメソッド以外はデフォルトでpublic
+# class User
+#     def yo
+#         'yo'
+#     end
+# end
+
+# privateキーワード以降はclass内部からしか呼べない
 class User
+    private # これ
     def yo
         'yo'
     end
