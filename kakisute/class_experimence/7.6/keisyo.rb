c7f9260556563028b1a3daca21399300dda3ceee
halllllll <h4l@yahoo.ne.jp>
2018-08-18 22:58:34 +0900

    method overload

--- kakisute/class_experimence/7.6/keisyo.rb
+++ kakisute/class_experimence/7.6/keisyo.rb
@@ -5,18 +5,25 @@ class Product
         @name = name
         @price = price
     end
+    
+    def to_s
+        "name: #{name}, price: #{price}"
+    end
 end
-
-product = Product.new('movie', 1000)
 
 class DVD < Product
     attr_reader :running_time
     def initialize(name, price, running_time)
-        @name = name
-        @price = price
+        super(name, price)
         @running_time = running_time
+    end
+    def to_s
+        #"name: #{name}, price: #{price}, running_time: #{running_time}"
+        "#{super}, running_time: #{running_time}"
     end
 end
 
-dvd = DVD.new('A movie', 1000, 214)
-puts dvd.name, dvd.price, dvd.running_time
+product = Product.new('movie', 1000)
+puts product.to_s
+dvd = DVD.new('film', 24500, 100)
+puts dvd.to_s
