3b637be573403211c9a5209226abd3bca08203d5
halllllll <h4l@yahoo.ne.jp>
2018-08-20 20:00:35 +0900

    rubyの公開レベルのpublic, 以外と深いので多言語出身者は要注意だな

--- kakisute/class_experimence/7.7/koukailevel.rb
+++ kakisute/class_experimence/7.7/koukailevel.rb
@@ -15,4 +15,36 @@ class User
 end
 
 user = User.new
-puts user.yo
+# puts user.yo これはエラー（NoMethodError）になる。
+
+# rubyのprivateメソッド = 【レシーバを指定できない】（超重要）
+# つまりはサブクラスからはふつうに呼び出せる、したがってrubyのprivateメソッドはオーバーライド可能
+class Product
+    def to_s
+        "name: #{name}"
+    end
+
+    private 
+    # private付きだが、サブクラスでオーバーライド可能
+    # なので常にこれが呼び出されるとは限らない
+    def name
+        'default'
+    end
+end
+
+class DVD < Product
+    private
+    # over rideする
+    def name
+        'super ultra fuckin awesome sugoi movie'
+    end
+end
+
+product = Product.new
+# デフォルト
+puts product.to_s
+
+dvd = DVD.new
+# サブクラスによってover rideされたスーパークラスのprivate methodになる
+puts dvd.to_s
+
