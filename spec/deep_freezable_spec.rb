ef63e897875788333e78b888ca86be88db592850
halllllll <h4l@yahoo.ne.jp>
2018-08-31 14:39:18 +0900

    change matcher, more BDD taste

--- spec/deep_freezable_spec.rb
+++ spec/deep_freezable_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe "feeze immutable architecture" do
     subject(:table){{"Japan"=>"yen", "US"=>"doller", "India"=>"rupee"}}
     it "for array, itself and all elements are freezed." do
         expect(arr).to eq Team::COUNTRIES
-        expect(Team::COUNTRIES.frozen?). to eq true
-        expect(Team::COUNTRIES.all?{|country| country.frozen?}).to eq true
+        expect(Team::COUNTRIES.frozen?). to be true
+        expect(Team::COUNTRIES.all?{|country| country.frozen?}).to be true
     end
     it "for hash, all key and value, and, hash itself are freesed" do
         expect(table).to eq Bank::CURRENCIES
-        expect(Bank::CURRENCIES.frozen?).to eq true
-        expect(Bank::CURRENCIES.all?{|k, v| k.frozen? && v.frozen?}).to eq true
+        expect(Bank::CURRENCIES.frozen?).to be true
+        expect(Bank::CURRENCIES.all?{|k, v| k.frozen? && v.frozen?}).to be true
     end
 end
