d742d49570dbe7691c3b44aecf414f74e204e193
halllllll <h4l@yahoo.ne.jp>
2018-08-31 14:23:51 +0900

    first test: for array, itself and all elements are freezed

--- lib/DF/deep_freezable.rb
+++ lib/DF/deep_freezable.rb
@@ -1,4 +1,8 @@
 module DeepFreezable
     def deep_freeze(arr_or_hash)
+        arr_or_hash.each do |a|
+            a.freeze
+        end
+        arr_or_hash.freeze
     end
 end
--- lib/DF/team.rb
+++ lib/DF/team.rb
@@ -3,6 +3,5 @@ require "./lib/DF/deep_freezable"
 class Team
     extend DeepFreezable
 
-    # COUNTRIES = deep_freeze(["Japan", "US", "India"])
-    COUNTRIES = ["Japan", "US", "India"]
+    COUNTRIES = deep_freeze(["Japan", "US", "India"])
 end
