bb9ba244780290354f6cb92f35b46003e6832b16
halllllll <h4l@yahoo.ne.jp>
2018-09-01 11:11:46 +0900

    begin nest return

--- kakisute/exceptionhandling_experiment.rb
+++ kakisute/exceptionhandling_experiment.rb
@@ -19,3 +19,30 @@ rescue
 end
 
 puts "End"
+
+# begin ~ rescueで包まれている箇所から包まれていない箇所で発生したエラーの場合
+# 包まれている箇所までさかのぼってくれる
+# エラーが発生したところ以降の処理はされず、発生した時点でさかのぼる
+def method_1
+    puts "method_1 start"
+    begin
+        method_2
+    rescue
+        puts "どっかで例外が発生した"
+    end
+    puts "method_1 end"
+end
+
+def method_2
+    puts "method_2 start"
+    method_3
+    puts "method_2 end"
+end
+
+def method_3
+    puts "method_3 start"
+    3/0
+    puts "method_3 end"
+end
+
+method_1
