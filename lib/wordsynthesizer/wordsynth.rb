f0310895a14ec4110ff53b6c3867ebeac4279b5a
halllllll <h4l@yahoo.ne.jp>
2018-09-04 12:57:58 +0900

    complete WordSynth and all passed testcode 🍒

--- lib/wordsynthesizer/wordsynth.rb
+++ lib/wordsynthesizer/wordsynth.rb
@@ -1,4 +1,14 @@
 class WordSynth
-    def play(original_words)
+    def initialize
+        @effects = []
     end
+    def add_effect(effect)
+        @effects << effect
+    end
+    def play(original_word)
+        @effects.inject(original_word) do |w, e|
+            e.call(w)
+        end
+    end
+
 end
--- spec/wordsyntesizer_spec.rb
+++ spec/wordsyntesizer_spec.rb
@@ -2,7 +2,7 @@ require 'wordsynthesizer/wordsynth'
 require 'wordsynthesizer/effects'
 RSpec.describe "word synthesizerのテスト" do
     before do
-        @wordsynth = WordSynth.new
+        @synth = WordSynth.new
         @effects = Effects
         @dora = "Doraemon"
         @nemui = "nemui"
@@ -10,7 +10,7 @@ RSpec.describe "word synthesizerのテスト" do
     end
     context "Wordsynthクラス, Effectモジュールがそもそも読めるか" do
         it "should can create wordsynth class" do
-            expect(@wordsynth.class).to be WordSynth
+            expect(@synth.class).to be WordSynth
         end
         # こんな文法であってるのかは知らない
         it "should can read Effects module" do 
@@ -34,6 +34,35 @@ RSpec.describe "word synthesizerのテスト" do
         it "invoke Loud method" do
             expect(@effects.loud(2).call(@dora)).to eq "DORAEMON!!"
             expect(@effects.loud(3).call(@thap)).to eq "THIS!!! IS!!! A!!! PEN!!!"
+        end
+    end
+
+    context WordSynth do
+        it "WordSynthクラスのplayメソッド" do
+            expect(@synth.play(@dora)).to eq @dora
+        end
+        it "echo effect" do
+            @synth.add_effect(Effects.echo(2))
+            expect(@synth.play(@dora)).to eq "DDoorraaeemmoonn"
+        end
+        it "reverse effect" do
+            @synth.add_effect(Effects.reverse)
+            expect(@synth.play(@thap)).to eq "sihT si a nep"
+        end
+        it "apply some effects nemui" do
+            @synth.add_effect(Effects.echo(2))
+            @synth.add_effect(Effects.loud(2))
+            @synth.add_effect(Effects.reverse)
+            @synth.add_effect(Effects.echo(3))
+            expect(@synth.play(@nemui)).to eq "!!!!!!IIIIIIUUUUUUMMMMMMEEEEEENNNNNN"
+        end
+        it "apply some effects thap" do
+            @synth.add_effect(Effects.reverse)
+            @synth.add_effect(Effects.echo(3))
+            @synth.add_effect(Effects.reverse)
+            @synth.add_effect(Effects.loud(3))
+            @synth.add_effect(Effects.reverse)
+            expect(@synth.play(@thap)).to eq "!!!SSSIIIHHHTTT !!!SSSIII !!!AAA !!!NNNEEEPPP"
         end
     end
 end
