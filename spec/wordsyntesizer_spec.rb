2e752b6ec12c97ba75b4ae466eac46466a4aaa7a
halllllll <h4l@yahoo.ne.jp>
2018-09-04 11:38:59 +0900

    add test for creatable Effects module and go on

--- spec/wordsyntesizer_spec.rb
+++ spec/wordsyntesizer_spec.rb
@@ -1,16 +1,17 @@
 require 'wordsynthesizer/wordsynth'
 require 'wordsynthesizer/effects'
 RSpec.describe "word synthesizerのテスト" do
-    # before節 -- example(itブロック)の前に必ず呼ばれる初期化処理
-    # 最上位のdescribe直下のbeforeで定義したやつはその下のdescribeの中のexampleでも使用可能
     before do
         @wordsynth = WordSynth.new
         @effects = Effects
     end
     context "Wordsynthクラス, Effectモジュールがそもそも読めるか" do
-        it "should can create wordsynth" do
-            # expect -> テストの最小単位
+        it "should can create wordsynth class" do
             expect(@wordsynth.class).to be WordSynth
+        end
+        # こんな文法であってるのかは知らない
+        it "should can read Effects module" do 
+            expect(@effects.inspect).to eq "Effects"
         end
     end
 end
